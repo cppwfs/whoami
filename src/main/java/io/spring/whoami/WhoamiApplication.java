6dbbf25abc7aa84a0ee793730c8d10912cfda7fd
Glenn Renfro <grenfro@pivotal.io>
2019-09-06 15:45:31 -0400

    Initial Checkin

---
+++ src/main/java/io/spring/whoami/WhoamiApplication.java
@@ -0,0 +1,27 @@
+package io.spring.whoami;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableTask
+public class WhoamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoamiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				System.out.println("**************VERSION ONE***********");
+			}
+		};
+	}
+
+}
